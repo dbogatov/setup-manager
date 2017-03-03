5a755c77bacd8707697c6f9f75470c99b16bbbea
Dmytro Bogatov <dmytro@dbogatov.org>
2017-03-02 20:16:19 -0500

    Fix.

--- users.sh
+++ users.sh
@@ -6,9 +6,13 @@ function add_user {
 
 	echo $1:$2 | sudo chpasswd
 
+	if $3 then
+		gpasswd -a dbogatov sudo
+	fi
+
 	echo_success "User $1 added"
 }
 
 function setup_users {
-	id -u $USERNAME &>/dev/null && echo_success "User $USERNAME exists" || add_user $USERNAME $PASSWORD
+	id -u $USERNAME &>/dev/null && echo_success "User $USERNAME exists" || add_user $USERNAME $PASSWORD true
 }
