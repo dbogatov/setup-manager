e923c47f1953b68e3722cb3043634283956e4a5b
Dmytro Bogatov <dmytro@dbogatov.org>
2017-03-02 21:10:28 -0500

    Fix.

--- docker.sh
+++ docker.sh
@@ -3,15 +3,22 @@ source functions.sh
 function install_docker {
 
 	echo_info "Installing docker"
+	inc_indent
 
-	wget -qO- https://get.docker.com/ | sh
+		echo_info "Installing Docker from get.docker.com script"
+		
+		wget -qO- https://get.docker.com/ | sh
 
-	sudo usermod -aG docker $(whoami)
+		echo_info "Adding user to docker group"
+		sudo usermod -aG docker $(whoami)
 
-	apt_install python-pip
+		echo_info "Installing PIP"
+		apt_install python-pip
 
-	sudo pip install docker-compose
+		echo_info "Installing Docker Compose"
+		sudo pip install docker-compose
 
+	dec_indent
 	echo_success "Installed docker"
 }
 
