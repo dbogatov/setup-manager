23053ef1ac4ab22e5809dfbc987f712c1a292a5e
Dmytro Bogatov <dmytro@dbogatov.org>
2017-03-02 20:35:32 -0500

    Fix.

--- emacs.sh
+++ emacs.sh
@@ -8,6 +8,8 @@ function install_emacs {
 
 	mkdir -p /root/.emacs.d/
 	cp config/emacs/init.el /root/.emacs.d/init.el
+
+	mkdir -p /home/$USERNAME/.emacs.d/
 	cp config/emacs/init.el /home/$USERNAME/.emacs.d/init.el
 
 	echo_success "Installed emacs"
