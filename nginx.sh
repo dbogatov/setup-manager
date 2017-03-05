bae53c280670404d7c519c624931194cf26a85c9
Dmytro Bogatov <dmytro@dbogatov.org>
2017-03-05 18:34:01 -0500

    Fix.

--- nginx.sh
+++ nginx.sh
@@ -8,8 +8,8 @@ function setup_nginx {
 
 	mkdir -p tmp
 	cd tmp
-	curl --header "PRIVATE-TOKEN: $TOKEN" https://git.dbogatov.org/api/v4/projects/15/builds/artifacts/master/download?job=artifacts > nginx.zip
-	unzip nginx.zip
+	curl -s --header "PRIVATE-TOKEN: $TOKEN" https://git.dbogatov.org/api/v4/projects/15/builds/artifacts/master/download?job=artifacts > nginx.zip
+	unzip nginx.zip > /dev/null
 	sudo cp -r dist/* /etc/nginx/
 	cd ..
 	rm -rf tmp
--- postgres.sh
+++ postgres.sh
@@ -11,7 +11,7 @@ function setup_postgres {
 		-p 5432:5432 \
 		-e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
 		-e POSTGRES_USER=$POSTGRES_USER \
-		-e POSTGRES_DB=$POSTGRES_DB
+		-e POSTGRES_DB=$POSTGRES_DB \
 		-d \
 		postgres:alpine
 
