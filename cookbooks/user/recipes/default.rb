d8e428eaf85e8b2f8b694c3a08ba8c94d19ff4d5
Dmytro Bogatov <dmytro@dbogatov.org>
2017-03-16 14:47:38 -0400

    Little change

--- cookbooks/user/recipes/default.rb
+++ cookbooks/user/recipes/default.rb
@@ -7,11 +7,17 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "postgresql::server"
+include_recipe "sudo"
 
 gem_package "ruby-shadow" do
 	action :install
 end
 
+package "openssl" do
+	action :install
+end
+
 user "Create admin user" do
+	password `openssl passwd -1 #{data_bag_item("user", "admin")["password"]}`
+	username node["user"]["me"]
 end
