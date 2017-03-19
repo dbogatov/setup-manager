213de7913813ec7472a583357239e4892799d94e
Dmytro Bogatov <dmytro@dbogatov.org>
2017-03-19 14:53:18 -0400

    Little change

--- cookbooks/databases/attributes/default.rb
+++ cookbooks/databases/attributes/default.rb
@@ -1,2 +1,2 @@
 default["postgresql"]["database_name"] = "testobase"
-node["postgresql"]["password"]["postgres"] = data_bag_item("postgres", "user")["password"]
+default["postgresql"]["password"]["postgres"] = data_bag_item("postgres", "user")["password"]
