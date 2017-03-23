81955a7d12251bb7f4401ca00ad6e4f520aaf0ae
Dmytro Bogatov <dmytro@dbogatov.org>
2017-03-23 18:08:55 -0400

    Little change

--- cookbooks/ultimate/metadata.rb
+++ cookbooks/ultimate/metadata.rb
@@ -6,6 +6,6 @@ description      "Installs/Configures projects"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "0.1.0"
 
-depends          "docker", "~> 2.0"
+depends          "docker"
 depends          "sudo"
 
