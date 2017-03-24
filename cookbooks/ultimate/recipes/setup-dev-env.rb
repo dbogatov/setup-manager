87e51176a2cadde3b9c3133f8f2000e6c73b256b
Dmytro <dbogatov@wpi.edu>
2017-03-24 15:30:13 -0400

    Little change

--- cookbooks/ultimate/recipes/setup-dev-env.rb
+++ cookbooks/ultimate/recipes/setup-dev-env.rb
@@ -12,30 +12,30 @@ user = node["user"]["me"]
 home = node["etc"]["passwd"][user]["dir"] # Chef DSL
 
 directory "#{home}/.emacs.d" do
-    owner user
-    group user
-    action :create
-    recursive true
+	owner user
+	group user
+	action :create
+	recursive true
 end
 
 cookbook_file "#{home}/.emacs.d/init.el" do
-    source "init.el"
+	source "init.el"
 end
 
 directory "#{home}/.byobu" do
-    owner user
-    group user
-    action :create
-    recursive true
+	owner user
+	group user
+	action :create
+	recursive true
 end
 
 execute "Enable byobu" do
-    cwd home
-    user user
-    command "/usr/bin/byobu-enable"
+	# cwd home
+	environment "HOME" => home
+	user user
+	command "/usr/bin/byobu-enable"
 end
 
 cookbook_file "#{home}/.byobu/status" do
-    source "status"
+	source "status"
 end
-
