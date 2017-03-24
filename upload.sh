2ce25ece78e6109f0be30f0178d7c87fd77759c4
Dmytro <dbogatov@wpi.edu>
2017-03-24 14:13:30 -0400

    Little change

--- upload.sh
+++ upload.sh
@@ -2,6 +2,7 @@
 
 git add . && git commit -m "Little change"
 
+knife data bag from file ultimate .chef/encrypted_data_bag.json --secret-file .chef/encrypted_data_bag_secret &
 knife role from file roles/*.rb &
 knife cookbook upload -a -d &
 git push &
