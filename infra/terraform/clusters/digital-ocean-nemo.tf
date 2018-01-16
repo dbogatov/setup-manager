ab18e779c7869c409f5e460231efc6285c99cb77
Dmytro Bogatov <dmytro@dbogatov.org>
2018-01-16 15:46:53 -0500

    Fix.

--- infra/terraform/clusters/digital-ocean-nemo.tf
+++ infra/terraform/clusters/digital-ocean-nemo.tf
@@ -15,9 +15,9 @@ module "digital-ocean-nemo" {
   cluster_name     = "nemo"
   image            = "coreos-stable"
   controller_count = 1
-  controller_type  = "2gb"
+  controller_type  = "s-1vcpu-1gb"
   worker_count     = 1
-  worker_type      = "512mb"
+  worker_type      = "s-1vcpu-2gb"
   ssh_fingerprints = ["df:a9:7f:e1:e5:e8:c7:3e:2c:c3:a9:ac:7c:bd:e7:a6"]
 
   # output assets dir
