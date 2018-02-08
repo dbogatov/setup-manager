852e4261d8bab46d01a95a301d3da5dda48f8292
Dmytro Bogatov <dmytro@dbogatov.org>
2018-02-08 01:53:15 -0500

    Fix.

--- infra/build-services.sh
+++ infra/build-services.sh
@@ -20,8 +20,8 @@ do
 
 	cp sources/service/{ingress,service,deployment}.yaml services/$service
 
-	sed -i '' -e "s#__NAME__#$service#g" services/$service/{ingress,service,deployment}.yaml
-	sed -i '' -e "s#__IMAGE__#${SERVICES[${service}]}#g" services/$service/{ingress,service,deployment}.yaml
+	sed -i -e "s#__NAME__#$service#g" services/$service/{ingress,service,deployment}.yaml
+	sed -i -e "s#__IMAGE__#${SERVICES[${service}]}#g" services/$service/{ingress,service,deployment}.yaml
 done
 
 ### Dashboard OAuth
@@ -33,13 +33,13 @@ cp sources/dashboard-auth/*.yaml services/dashboard-auth/
 
 COOKIE_SECRET=$(python -c 'import os,base64; print base64.b64encode(os.urandom(16))')
 
-sed -i '' -e "s#__OAUTH2_PROXY_COOKIE_SECRET__#$COOKIE_SECRET#g" services/dashboard-auth/oauth2-proxy.yaml
-sed -i '' -e "s#__OAUTH2_PROXY_CLIENT_SECRET__#$OAUTH2_PROXY_CLIENT_SECRET#g" services/dashboard-auth/oauth2-proxy.yaml
+sed -i -e "s#__OAUTH2_PROXY_COOKIE_SECRET__#$COOKIE_SECRET#g" services/dashboard-auth/oauth2-proxy.yaml
+sed -i -e "s#__OAUTH2_PROXY_CLIENT_SECRET__#$OAUTH2_PROXY_CLIENT_SECRET#g" services/dashboard-auth/oauth2-proxy.yaml
 
 DASHBOARD_TOKEN=$(kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}') | grep token: )
 DASHBOARD_TOKEN="${DASHBOARD_TOKEN:7:${#DASHBOARD_TOKEN}}"
 
-sed -i '' -e "s#__DASHBOARD_TOKEN__#$DASHBOARD_TOKEN#g" services/dashboard-auth/ingreses.yaml
-sed -i '' -e "s#Bearer      #Bearer #g" services/dashboard-auth/ingreses.yaml
+sed -i -e "s#__DASHBOARD_TOKEN__#$DASHBOARD_TOKEN#g" services/dashboard-auth/ingreses.yaml
+sed -i -e "s#Bearer      #Bearer #g" services/dashboard-auth/ingreses.yaml
 
 echo "Done!"
